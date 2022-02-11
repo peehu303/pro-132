954a2b90f0410ab194af941c0d8a468517c87a48
peehu303 <84406912+peehu303@users.noreply.github.com>
2022-02-11 14:08:57 +0530

    Add files via upload

---
+++ kitchen.js
@@ -0,0 +1,29 @@
+let img;
+let status;
+function preload() {
+    img = loadImage("kitchen.jpg");
+}
+function setup() {
+    canvas = createCanvas(650, 500);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Object";
+}
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+function draw() {
+    image(img, 0, 0, 650, 500);
+}
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+    }
+    if (status == true) {
+        document.getElementById("status").innerHTML = "Status: Object Detected";
+    }
+}
